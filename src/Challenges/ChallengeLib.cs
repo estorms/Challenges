7cce2dc8826a2f10cb608a2e6056419901ae09ab
Elizabeth Sanger <libsang@gmail.com>
2017-01-21 09:50:29 -0600

    first commit

---
+++ src/Challenges/ChallengeLib.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class ChallengeLib
+    {
+        public int Fib(int n) {
+            int a = 0;
+            int b = 1;
+            int c;
+            for (var i = 0; i <=n;  i++)
+            {
+                c = a;
+                a = b;
+                b = c + a;
+            }
+            return a;
+        }
+
+        public bool IsPalindrome(string testString)
+        {
+            return testString.SequenceEqual(testString.Reverse());
+        }
+
+        public void FizzBuzz ()
+        {
+            for (var k = 0; k <=25; k++)
+            {
+                bool fizz = k % 5 == 0;
+                bool buzz = k % 3 == 0;
+                if (fizz && buzz)
+                {
+                    Console.WriteLine("fizzbuzz");
+                  
+                    }
+                    else if (fizz)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (buzz)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(k);
+                }
+                }
+            }
+        }
+    
+}
---
+++ src/Challenges/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ChallengeLib challengeLib = new ChallengeLib();
+            challengeLib.FizzBuzz();
+            Console.WriteLine(challengeLib.IsPalindrome("koala"));
+            Console.WriteLine(challengeLib.IsPalindrome("kayak"));
+            for (var j = 0; j <=15; j++)
+            {
+                Console.WriteLine(challengeLib.Fib(j));
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
